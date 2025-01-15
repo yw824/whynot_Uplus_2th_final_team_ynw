024499c4fbecca3045c593951a28292de5ce640e
yw824 <yongwoo25@gmail.com>
2025-01-15 12:37:53 +0900

    feat: Model로 Query 수행하는 logic 분리 및 AWS RDS와 연결
    - Query to LLM 코드 : model_query.py
    - RDS 연결 -> credentials.py (.gitignore)
    	- host, endpoint 등 민감한 정보 있음

--- scripts/pinecone_db.py
+++ scripts/pinecone_db.py
@@ -7,19 +7,33 @@ from langchain_community.document_loaders.csv_loader import CSVLoader
 from langchain_pinecone import PineconeVectorStore
 from pinecone import Pinecone
 
+# 초기 데이터 설정
+load_dotenv()
 
-def split_document(file_path):
+def split_document(file_path: str = "../data/final_qa_list.csv"):
+    """
+    '../data/final_qa_list.csv'
+    document = split_document(file_path)
+    """
+
     text_splitter = RecursiveCharacterTextSplitter(
         chunk_size=1500,
         chunk_overlap=200,
     )
 
-    file_loader = CSVLoader(file_path=file_path, encoding='utf-8-sig')
-    file_split_list = file_loader.load_and_split(text_splitter=text_splitter)
-    return file_split_list
+    try:
+        file_loader = CSVLoader(file_path=file_path, encoding='utf-8-sig')
+        file_split_list = file_loader.load_and_split(text_splitter=text_splitter)
+        return file_split_list
+    except Exception as e:
+        print(f"Error reading CSV file: {e}")
 
 
-def init_pinecone_database(embedding_model, index_name, document):
+def init_pinecone_database(document, embedding_model: str ="solar-embedding-1-large", index_name: str ='upstage-index'):
+    """
+    database = init_pinecone_database(document=document, embedding_model=embedding_model, index_name=index_name)
+    """
+
     embedding = UpstageEmbeddings(model=embedding_model, api_key=os.getenv("UPSTAGE_API_KEY"))
 
     pc = Pinecone(api_key=os.getenv("PINECONE_API_KEY"))
@@ -38,20 +52,3 @@ def init_pinecone_database(embedding_model, index_name, document):
         # print(f"{index_name} is exist")
         database = PineconeVectorStore.from_existing_index(index_name=index_name, embedding=embedding)
         return database
-
-
-# 초기 데이터 설정
-load_dotenv()
-
-file_path = '../data/final_qa_list.csv'
-embedding_model = 'solar-embedding-1-large'
-index_name = 'upstage-index'
-
-document = split_document(file_path)
-
-database = init_pinecone_database(embedding_model=embedding_model, index_name=index_name, document=document)
-
-
-
-
-
